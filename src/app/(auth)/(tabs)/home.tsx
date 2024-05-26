cd99fbcde23e831bb58360985602b9bf72f45f85
Utkarsh Verma <108287933+Utkarshvr@users.noreply.github.com>
2024-05-26 22:35:13 +0530

    Chat Screen Setup

--- src/app/(auth)/(tabs)/home.tsx
+++ src/app/(auth)/(tabs)/home.tsx
@@ -1,20 +1,5 @@
-import { StyleSheet, Text, View } from "react-native";
-import { useUser } from "@clerk/clerk-expo";
-import FriendsFAB from "@/components/buttons/FriendsFAB";
+import ChatsScreen from "@/Screens/(auth)/(tabs)/ChatsScreen";
 
 export default function home() {
-  const { user, isLoaded } = useUser();
-
-  return (
-    <View className="flex-1 items-center justify-center bg-neutral-100 dark:bg-neutral-950 ">
-      <Text className="text-neutral-950 dark:text-neutral-100">Home</Text>
-      <Text className="text-neutral-950 dark:text-neutral-100">
-        Hi, {user?.username}
-      </Text>
-
-      <FriendsFAB />
-    </View>
-  );
+  return <ChatsScreen />;
 }
-
-const styles = StyleSheet.create({});
